981b4f76062f2ebd1912de98befcaaf08015354e
tsteelematc <tsteele@madisoncollege.edu>
2025-10-15 14:54:26 -0500

    Blah, fun, but, blah, hard...

--- src/app/sw-planet-films/sw-planet-films.ts
+++ src/app/sw-planet-films/sw-planet-films.ts
@@ -1,6 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SwPlanetsService } from '../sw-planets.service';
 import { PlanetWithFilmCount } from '../../sw-planet-model';
+
+type PlanetDisplay = {
+  rank: string;
+  name: string;
+  filmCount: number;
+  mostFilms: boolean;
+};
 
 @Component({
   selector: 'app-sw-planet-films',
@@ -8,9 +15,41 @@ import { PlanetWithFilmCount } from '../../sw-planet-model';
   templateUrl: './sw-planet-films.html',
   styleUrl: './sw-planet-films.css'
 })
-export class SwPlanetFilms {
+export class SwPlanetFilms implements OnInit {
 
   private planetService = inject(SwPlanetsService);
 
-  protected planetsToDisplay: PlanetWithFilmCount[] = this.planetService.getPlanetFilmDataForDisplay();
+  protected planetsToDisplay: PlanetDisplay[] = [];
+
+  ngOnInit(): void {
+
+    const planets = this.planetService.getPlanetFilmDataForDisplay();
+
+    const maxFilms = Math.max(
+      ...planets.map(
+        x => x.filmCount
+      )
+    );
+    
+    this.planetsToDisplay = planets
+      .map(
+        x => {
+          const firstIndex = planets.findIndex(
+            y => y.filmCount === x.filmCount
+          );
+
+          const lastIndex = planets.findLastIndex(
+            y => y.filmCount === x.filmCount
+          );
+
+          return {
+            rank: `${firstIndex === lastIndex ? '' : 'T'}${firstIndex + 1}`,
+            name: x.name,
+            filmCount: x.filmCount,
+            mostFilms: x.filmCount === maxFilms,
+          };
+        }
+      )
+  ;
+  }
 }
