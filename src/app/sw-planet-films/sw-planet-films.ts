ac6160ce1f2189b4dc8b8e71c8e4cbd70a5043f3
tsteelematc <tsteele@madisoncollege.edu>
2025-10-18 14:03:58 -0500

    All none, meh...

--- src/app/sw-planet-films/sw-planet-films.ts
+++ src/app/sw-planet-films/sw-planet-films.ts
@@ -1,10 +1,18 @@
 import { Component, inject, OnInit, signal, WritableSignal, computed } from '@angular/core';
 import { SwPlanetsService } from '../sw-planets.service';
 import { PlanetWithFilmCount } from '../../sw-planet-model';
-import { MatCard, MatCardContent, MatCardFooter, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardFooter,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle,
+} from '@angular/material/card';
 import { MatBadge } from '@angular/material/badge';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
 
 type PlanetDisplay = {
   rank: string;
@@ -17,86 +25,73 @@ type PlanetDisplay = {
 @Component({
   selector: 'app-sw-planet-films',
   imports: [
-    MatCard, 
-    MatCardTitle, 
-    MatCardContent, 
-    MatCardHeader, 
-    MatCardSubtitle, 
-    MatCardFooter, 
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    MatCardHeader,
+    MatCardSubtitle,
+    MatCardFooter,
     MatBadge,
     MatCheckbox,
     FormsModule,
+    MatButton,
   ],
   templateUrl: './sw-planet-films.html',
-  styleUrl: './sw-planet-films.css'
+  styleUrl: './sw-planet-films.css',
 })
 export class SwPlanetFilms implements OnInit {
-
   private planetService = inject(SwPlanetsService);
 
   protected planetsToDisplay: WritableSignal<PlanetDisplay[]> = signal([]);
 
   protected readonly selectedFavesCount = computed(
-    () => this.planetsToDisplay().filter(
-      x => x.fave
-    ).length
+    () => this.planetsToDisplay().filter((x) => x.fave).length
   );
 
-  protected readonly sumOfFilmCounts = computed(
-    () => this.planetsToDisplay()
-      .filter(
-        x => x.fave
-      )
-      .reduce(
-        (acc, x) => acc + x.filmCount,
-        0,
-      )
+  protected readonly sumOfFilmCounts = computed(() =>
+    this.planetsToDisplay()
+      .filter((x) => x.fave)
+      .reduce((acc, x) => acc + x.filmCount, 0)
   );
 
   ngOnInit(): void {
-
     const planets = this.planetService.getPlanetFilmDataForDisplay();
 
-    const maxFilms = Math.max(
-      ...planets.map(
-        x => x.filmCount
-      )
+    const maxFilms = Math.max(...planets.map((x) => x.filmCount));
+
+    this.planetsToDisplay.update((prev) =>
+      planets.map((x) => {
+        const firstIndex = planets.findIndex((y) => y.filmCount === x.filmCount);
+
+        const lastIndex = planets.findLastIndex((y) => y.filmCount === x.filmCount);
+
+        return {
+          rank: `${firstIndex === lastIndex ? '' : 'T'}${firstIndex + 1}`,
+          name: x.name,
+          filmCount: x.filmCount,
+          mostFilms: x.filmCount === maxFilms,
+          fave: false,
+        };
+      })
     );
-    
-    this.planetsToDisplay.update(
-      prev => planets
-        .map(
-          x => {
-            const firstIndex = planets.findIndex(
-              y => y.filmCount === x.filmCount
-            );
-
-            const lastIndex = planets.findLastIndex(
-              y => y.filmCount === x.filmCount
-            );
-
-            return {
-              rank: `${firstIndex === lastIndex ? '' : 'T'}${firstIndex + 1}`,
-              name: x.name,
-              filmCount: x.filmCount,
-              mostFilms: x.filmCount === maxFilms,
-              fave: false,
-            };
-          }
-        )
-    )
-  ;
   }
 
-  protected faveToggled = (p: PlanetDisplay) => {console.log("here"); return this.planetsToDisplay.update(
+  protected faveToggled = (p: PlanetDisplay) => {
+    console.log('here');
+    return this.planetsToDisplay.update((prev) =>
+      prev.map((x) => ({
+        ...x,
+        fave: x === p ? !x.fave : x.fave,
+      }))
+    );
+  };
+
+  protected faveAll = (check: boolean) => this.planetsToDisplay.update(
     prev => prev.map(
       x => ({
         ...x,
-        fave: x === p 
-          ? !x.fave 
-          : x.fave,
+        fave: check
       })
     )
-  )
-}
+  );
 }
