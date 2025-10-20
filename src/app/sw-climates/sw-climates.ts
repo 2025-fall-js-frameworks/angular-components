55419a9fa05938bb97249e1bd266785eabbb1f6f
tsteelematc <tsteele@madisoncollege.edu>
2025-10-20 17:20:49 -0500

    ng generate component sw-climates

---
+++ src/app/sw-climates/sw-climates.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sw-climates',
+  imports: [],
+  templateUrl: './sw-climates.html',
+  styleUrl: './sw-climates.css'
+})
+export class SwClimates {
+
+}
