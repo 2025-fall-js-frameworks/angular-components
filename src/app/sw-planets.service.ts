1f212f74b54d29bf61ebe46a0bda724f400aecf0
droc101 <37421449+droc101@users.noreply.github.com>
2025-10-21 13:58:14 -0500

    Call service from films component

--- src/app/sw-planets.service.ts
+++ src/app/sw-planets.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class SwPlanetsService {
-  
+	public foo = () => {
+		console.log('SwPlanetsService::foo called!');
+		return 'bar123';
+	};
 }
