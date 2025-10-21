39f2cba3ec697d8f2b4a113be35c97e340a6e313
Austin-Spriggs <aspriggs@madisoncollege.edu>
2025-10-21 13:59:09 -0500

    added planet service functionality

--- src/app/sw-planet-films/sw-planet-films.ts
+++ src/app/sw-planet-films/sw-planet-films.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SwPlanetsService } from '../sw-planets.service';
 
 @Component({
-  selector: 'app-sw-planet-films',
-  imports: [],
-  templateUrl: './sw-planet-films.html',
-  styleUrl: './sw-planet-films.css'
+	selector: 'app-sw-planet-films',
+	imports: [],
+	templateUrl: './sw-planet-films.html',
+	styleUrl: './sw-planet-films.css'
 })
-export class SwPlanetFilms {
 
+export class SwPlanetFilms {
+	private planetSvc = inject(SwPlanetsService);
+
+	public readonly bar = this.planetSvc.foo();
 }
