18e861698f92a13a50b057023732552e011fcc0c
yyang22-JS Frameworks <yyang22@madisoncollege.edu>
2025-10-23 13:03:09 -0500

    Cleaned up the plumbing...
    Yes, I write code I know I will delete. get over it :-O

--- src/app/sw-planet-films/sw-planet-films.ts
+++ src/app/sw-planet-films/sw-planet-films.ts
@@ -2,14 +2,11 @@ import { Component, inject } from '@angular/core';
 import { SwPlanetsService } from '../sw-planets.service';
 
 @Component({
-  selector: 'app-sw-planet-films',
-  imports: [],
-  templateUrl: './sw-planet-films.html',
-  styleUrl: './sw-planet-films.css'
+	selector: 'app-sw-planet-films',
+	imports: [],
+	templateUrl: './sw-planet-films.html',
+	styleUrl: './sw-planet-films.css',
 })
 export class SwPlanetFilms {
-
-  private planetSvc = inject(SwPlanetsService);
-
-  protected readonly bar = this.planetSvc.foo();
+	private planetSvc = inject(SwPlanetsService);
 }
