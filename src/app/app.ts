f347e635877abec666e322a2dcca8b33619d4864
tsteelematc <tsteele@madisoncollege.edu>
2025-10-23 11:27:03 -0500

    Generated component and added it to app root to see it work
    ng generate component tsteele-my-component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { SwPlanetFilms } from './sw-planet-films/sw-planet-films';
+import { TsteeleMyComponent } from './tsteele-my-component/tsteele-my-component';
 
 @Component({
   selector: 'app-root',
-  imports: [SwPlanetFilms],
+  imports: [SwPlanetFilms, TsteeleMyComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
