06f3baf7f0bc0d8279c66ce0c27ca587d9806ef4
Austin-Spriggs <aspriggs@madisoncollege.edu>
2025-10-23 13:59:43 -0500

    added new component for planets with pop data
    included imports in app.ts, app.html, and new function in aw-planets.services.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { SwPlanetFilms } from './sw-planet-films/sw-planet-films';
+import { SwPlanetPopulation } from './sw-planet-population/sw-planet-population';
 
 @Component({
 	selector: 'app-root',
-	imports: [SwPlanetFilms],
+	imports: [SwPlanetFilms, SwPlanetPopulation],
 	templateUrl: './app.html',
 	styleUrl: './app.css',
 })
