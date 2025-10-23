69432447d710e3abf316579a95079ed92cfa0bc8
yyang22-JS Frameworks <yyang22@madisoncollege.edu>
2025-10-23 14:04:54 -0500

    Added component into app.ts and app.html
    1. app.ts
    imports: yyang22mycomponent
    
    2. app.html
    <app-yyang22-my-component></app-yyang22-my-component>

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { SwPlanetFilms } from './sw-planet-films/sw-planet-films';
+import { Yyang22MyComponent } from './yyang22-my-component/yyang22-my-component';
 
 @Component({
 	selector: 'app-root',
-	imports: [SwPlanetFilms],
+	imports: [SwPlanetFilms, Yyang22MyComponent],
 	templateUrl: './app.html',
 	styleUrl: './app.css',
 })
