4e5397a8e6edfc5a81d1b8a1c0d1ad9aeddb1c55
Matthew Nguyen <mnguyen4@madisoncollege.edu>
2025-10-23 13:03:05 -0500

    Cleaned up the plumbing...
    Yes, I write code I know I will delete, get over it ; - O

--- src/app/sw-planets.service.ts
+++ src/app/sw-planets.service.ts
@@ -3,9 +3,4 @@ import { Injectable } from '@angular/core';
 @Injectable({
 	providedIn: 'root',
 })
-export class SwPlanetsService {
-	public foo = () => {
-		console.log('svc.foo was called ! ! !');
-		return "And that's I don't go to Denny's anymore!";
-	};
-}
+export class SwPlanetsService {}
