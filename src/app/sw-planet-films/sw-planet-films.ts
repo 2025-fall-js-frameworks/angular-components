9a1deb1d857f80b2cc7530aaaf5c6fa12d275099
tsteelematc <tsteele@madisoncollege.edu>
2025-10-24 13:42:27 -0500

    Showing on a MD card...
    And some padding in app...
    
    Next, display table ? ? ?

--- src/app/sw-planet-films/sw-planet-films.ts
+++ src/app/sw-planet-films/sw-planet-films.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { SwPlanetsService } from '../sw-planets.service';
 import { PlanetWithFilmCount } from '../../sw-planet-model';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 
 @Component({
 	selector: 'app-sw-planet-films',
-	imports: [],
+	imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent],
 	templateUrl: './sw-planet-films.html',
 	styleUrl: './sw-planet-films.css',
 })
