452513a41d462e7deee3cd3d50a27980f9a9bc3e
tsteelematc <tsteele@madisoncollege.edu>
2025-10-24 17:21:35 -0500

    Hmm, tricky woo with paginator and signals oh my...

--- src/app/sw-planet-films/sw-planet-films.ts
+++ src/app/sw-planet-films/sw-planet-films.ts
@@ -1,42 +1,67 @@
-import { AfterViewInit, Component, inject, viewChild, ViewChild } from '@angular/core';
+import {
+	AfterViewInit,
+	Component,
+	computed,
+	inject,
+	signal,
+	viewChild,
+	ViewChild,
+	WritableSignal,
+} from '@angular/core';
 import { SwPlanetsService } from '../sw-planets.service';
 import { PlanetWithFilmCount } from '../../sw-planet-model';
-import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 type PlanetForDisplayAndBehavior = PlanetWithFilmCount & { fave: boolean };
 
 @Component({
 	selector: 'app-sw-planet-films',
-	imports: [
-		MatCard,
-		MatCardHeader,
-		MatCardTitle,
-		MatCardContent,
-		MatTableModule,
-		MatPaginatorModule,
-	],
+	imports: [MatCardModule, MatTableModule, MatPaginatorModule, MatCheckboxModule],
 	templateUrl: './sw-planet-films.html',
 	styleUrl: './sw-planet-films.css',
 })
 export class SwPlanetFilms implements AfterViewInit {
 	private planetSvc = inject(SwPlanetsService);
 
-	protected readonly planetsToDisplay: PlanetForDisplayAndBehavior[] = this.planetSvc
-		.getPlanetFilmDataForDisplay()
-		.map((x) => ({
-			...x,
-			fave: false,
-		}));
+	protected readonly planetsToDisplay: PlanetWithFilmCount[] =
+		this.planetSvc.getPlanetFilmDataForDisplay();
 
 	displayedColumns = ['fave', 'name', 'filmCount'];
 
-	dataSource = new MatTableDataSource<PlanetWithFilmCount>(this.planetsToDisplay);
-
+	protected dataSource: any;
 	private paginator = viewChild(MatPaginator);
 
+	protected planetsWithFaveBehavior: WritableSignal<PlanetForDisplayAndBehavior[]> = signal([]);
+
 	ngAfterViewInit() {
+		this.planetsWithFaveBehavior.update((prev) =>
+			this.planetsToDisplay.map((x) => ({
+				...x,
+				fave: false,
+			})),
+		);
+
+		this.dataSource = new MatTableDataSource<PlanetForDisplayAndBehavior>(
+			this.planetsWithFaveBehavior(),
+		);
+
 		this.dataSource.paginator = this.paginator();
 	}
+
+	toggleFave = (planetToToggle: PlanetForDisplayAndBehavior) => {
+		console.log(`here ${planetToToggle.name}`);
+		this.planetsWithFaveBehavior.update((prev) =>
+			prev.map((x) => ({
+				...x,
+				fave: x === planetToToggle ? !x.fave : x.fave,
+			})),
+		);
+
+		this.dataSource.data = this.planetsWithFaveBehavior();
+	};
+
+	faveCount = computed(() => this.planetsWithFaveBehavior().filter((x) => x.fave).length);
 }
