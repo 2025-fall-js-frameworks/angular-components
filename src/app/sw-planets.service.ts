70fe4b501fcf486f353053b27c0ec04b28479e22
furkansindar <fsindar@madisoncollege.edu>
2025-10-24 21:16:34 -0500

    DI(dependency injection)working at least the plumbing
    On thu we will finish this

--- src/app/sw-planets.service.ts
+++ src/app/sw-planets.service.ts
@@ -5,4 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class SwPlanetsService {
   
+    public foo = () => {
+      console.log("svc.foo was called !  ! !");
+      return "bar123456";
+    }
 }
