e3ad97d6bd9f9e9b342d82931a417f275d179d8a
matgeroina <157189615+matgeroina@users.noreply.github.com>
2025-10-27 21:39:45 -0500

    new component
    generated a new component

---
+++ src/app/akoroliova-my-component/akoroliova-my-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-akoroliova-my-component',
+  imports: [],
+  templateUrl: './akoroliova-my-component.html',
+  styleUrl: './akoroliova-my-component.css'
+})
+export class AkoroliovaMyComponent {
+
+}
