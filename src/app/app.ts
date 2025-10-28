efa02a52813464d99d6e0c39dc731405569b4744
tsteelematc <tsteele@madisoncollege.edu>
2025-10-28 11:03:29 -0500

    Renaming with 02 to hopefully have better rebase/merge demos in class today : - O
    Only hoping : - OO

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { SwPlanetFilms } from './sw-planet-films/sw-planet-films';
-import { TsteeleMyComponent } from './tsteele-my-component/tsteele-my-component';
+import { TsteeleMyComponent02 } from './tsteele-my-component-02/tsteele-my-component-02';
 
 @Component({
 	selector: 'app-root',
-	imports: [SwPlanetFilms, TsteeleMyComponent],
+	imports: [SwPlanetFilms, TsteeleMyComponent02],
 	templateUrl: './app.html',
 	styleUrl: './app.css',
 })
