523b1ad155548083394fe909f5be857f64ab67ad
Austin-Spriggs <aspriggs@madisoncollege.edu>
2025-10-28 13:35:14 -0500

    editor lost my file - re-upload

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { SwPlanetFilms } from './sw-planet-films/sw-planet-films';
 import { TsteeleMyComponent } from './tsteele-my-component/tsteele-my-component';
+import { SwPlanetPopulation } from './sw-planet-population/sw-planet-population';
 
 @Component({
   selector: 'app-root',
-  imports: [SwPlanetFilms, TsteeleMyComponent],
+  imports: [
+    SwPlanetFilms,
+    TsteeleMyComponent,
+    SwPlanetPopulation
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
