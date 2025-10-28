faff47275a94147fa17a0a1ca283ff7b2d5d3f45
Kimberg23 <imbergk23@gmail.com>
2025-10-28 13:24:10 -0500

    Moved my component to the bottom of the page...

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { SwPlanetFilms } from './sw-planet-films/sw-planet-films';
-import { MyComponent } from './kimberg/my-component/my-component';
 import { TsteeleMyComponent } from './tsteele-my-component/tsteele-my-component';
+import { MyComponent } from './kimberg/my-component/my-component';
+
 
 @Component({
 	selector: 'app-root',
