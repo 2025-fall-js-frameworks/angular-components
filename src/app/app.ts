1d31308e0474b28be515002784f5b7b6f6057204
tsteelematc <tsteele@madisoncollege.edu>
2025-10-29 15:37:28 -0500

    Component generated and mocked up...
    ng generate component tsteele-add-two-numbers
    
    Have to remember binding, blah, blah, blah... Maybe some signals too...

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { SwPlanetFilms } from './sw-planet-films/sw-planet-films';
+import { TsteeleAddTwoNumbers } from './tsteele-add-two-numbers/tsteele-add-two-numbers';
 
 @Component({
 	selector: 'app-root',
-	imports: [SwPlanetFilms],
+	imports: [SwPlanetFilms, TsteeleAddTwoNumbers],
 	templateUrl: './app.html',
 	styleUrl: './app.css',
 })
---
+++ src/app/tsteele-add-two-numbers/tsteele-add-two-numbers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tsteele-add-two-numbers',
+  imports: [],
+  templateUrl: './tsteele-add-two-numbers.html',
+  styleUrl: './tsteele-add-two-numbers.css'
+})
+export class TsteeleAddTwoNumbers {
+
+}
