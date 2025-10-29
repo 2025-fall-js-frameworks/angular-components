a6bbffa32bce523d577c99fa3f25ff887c2cba50
tsteelematc <tsteele@madisoncollege.edu>
2025-10-29 15:40:35 -0500

    Signal for the answer and update it with an equal button press...

--- src/app/tsteele-add-two-numbers/tsteele-add-two-numbers.ts
+++ src/app/tsteele-add-two-numbers/tsteele-add-two-numbers.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-tsteele-add-two-numbers',
-  imports: [],
-  templateUrl: './tsteele-add-two-numbers.html',
-  styleUrl: './tsteele-add-two-numbers.css'
+	selector: 'app-tsteele-add-two-numbers',
+	imports: [],
+	templateUrl: './tsteele-add-two-numbers.html',
+	styleUrl: './tsteele-add-two-numbers.css',
 })
 export class TsteeleAddTwoNumbers {
+	protected readonly answer = signal(4);
 
+	protected readonly addTwoNumbers = () => {
+		this.answer.update((prev) => 42);
+	};
 }
