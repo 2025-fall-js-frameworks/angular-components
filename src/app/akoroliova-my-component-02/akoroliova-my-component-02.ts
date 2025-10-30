f5537ea2f4de56aa8ec61f5f54994b926ec140b0
matgeroina <157189615+matgeroina@users.noreply.github.com>
2025-10-30 13:27:26 -0500

    a working calculator component
    now calculator works when u click the equal button

--- src/app/akoroliova-my-component-02/akoroliova-my-component-02.ts
+++ src/app/akoroliova-my-component-02/akoroliova-my-component-02.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-akoroliova-my-component-02',
-  imports: [],
-  templateUrl: './akoroliova-my-component-02.html',
-  styleUrl: './akoroliova-my-component-02.css'
+	selector: 'app-akoroliova-my-component-02',
+	imports: [],
+	templateUrl: './akoroliova-my-component-02.html',
+	styleUrl: './akoroliova-my-component-02.css',
 })
 export class AkoroliovaMyComponent02 {
-
+	protected readonly numberOne = signal(2);
+	protected readonly numberTwo = signal(2);
+	protected readonly answer = signal(4);
+	protected readonly updateNumberone = (newNumber: number) =>
+		this.numberOne.update((prev) => newNumber);
+	protected readonly addNumbers = () => {
+		console.log('user press equal button');
+		this.answer.update((pre) => this.numberOne() + this.numberTwo());
+	};
 }
