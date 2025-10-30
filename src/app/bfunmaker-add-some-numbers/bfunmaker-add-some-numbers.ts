25cf4423a9d64619b7c6081850e8e8fcd218ffaa
bryson-fun <funmaker.bryson@gmail.com>
2025-10-30 12:56:02 -0500

    Generated my new component
    ng generate component bfunmaker-add-some-numbers

---
+++ src/app/bfunmaker-add-some-numbers/bfunmaker-add-some-numbers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bfunmaker-add-some-numbers',
+  imports: [],
+  templateUrl: './bfunmaker-add-some-numbers.html',
+  styleUrl: './bfunmaker-add-some-numbers.css'
+})
+export class BfunmakerAddSomeNumbers {
+
+}
