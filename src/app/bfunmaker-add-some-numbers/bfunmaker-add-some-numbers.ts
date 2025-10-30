c157e3033e1cdddcd54a5f65ebf61c21d1eb7bbc
bryson-fun <funmaker.bryson@gmail.com>
2025-10-30 13:33:17 -0500

    Created Event Bindings for function calculators
    Added codes and created some event handling code for users when they enter numbers into input. As well as created functioning "Calculate" two numbers.

--- src/app/bfunmaker-add-some-numbers/bfunmaker-add-some-numbers.ts
+++ src/app/bfunmaker-add-some-numbers/bfunmaker-add-some-numbers.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-bfunmaker-add-some-numbers',
-  imports: [],
-  templateUrl: './bfunmaker-add-some-numbers.html',
-  styleUrl: './bfunmaker-add-some-numbers.css'
+	selector: 'app-bfunmaker-add-some-numbers',
+	imports: [],
+	templateUrl: './bfunmaker-add-some-numbers.html',
+	styleUrl: './bfunmaker-add-some-numbers.css',
 })
 export class BfunmakerAddSomeNumbers {
+	protected readonly firstNumberInput = signal(2);
+	protected readonly secondNumberInput = signal(2);
+	protected readonly answer = signal(4);
 
+	protected readonly updateFirstInput = (newNumber: number) =>
+		this.firstNumberInput.update((prev) => newNumber);
+
+	protected readonly addNumbers = () => {
+		console.log('User used calculate button');
+		this.answer.update((prev) => this.firstNumberInput() + this.secondNumberInput());
+	};
 }
