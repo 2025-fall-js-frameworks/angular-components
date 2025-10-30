f9dbef39939f48756a5fde5dccb3e159181d1552
matgeroina <157189615+matgeroina@users.noreply.github.com>
2025-10-30 12:59:32 -0500

    showing a new component

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,7 +8,7 @@ import { MyComponent } from './kimberg/my-component/my-component';
 import { AkoroliovaMyComponent } from './akoroliova-my-component/akoroliova-my-component';
 import { SwPlanetPopulation } from './sw-planet-population/sw-planet-population';
 import { TsteeleAddTwoNumbers } from './tsteele-add-two-numbers/tsteele-add-two-numbers';
-
+import { AkoroliovaMyComponent02 } from './akoroliova-my-component-02/akoroliova-my-component-02';
 @Component({
 	selector: 'app-root',
 	imports: [
@@ -21,7 +21,9 @@ import { TsteeleAddTwoNumbers } from './tsteele-add-two-numbers/tsteele-add-two-
 		Yyang22MyComponent,
 		MnguyenMyComponent,
 		TsteeleAddTwoNumbers,
+		AkoroliovaMyComponent02
 	],
+
 	templateUrl: './app.html',
 	styleUrl: './app.css',
 })
