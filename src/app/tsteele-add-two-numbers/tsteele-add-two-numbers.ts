4466b8dd201787c041411f44b0d2a7fd96f80144
tsteelematc <tsteele@madisoncollege.edu>
2025-10-30 13:27:25 -0500

    Component working...
    Next create the PR...

--- src/app/tsteele-add-two-numbers/tsteele-add-two-numbers.ts
+++ src/app/tsteele-add-two-numbers/tsteele-add-two-numbers.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-tsteele-add-two-numbers',
-  imports: [],
-  templateUrl: './tsteele-add-two-numbers.html',
-  styleUrl: './tsteele-add-two-numbers.css'
+	selector: 'app-tsteele-add-two-numbers',
+	imports: [],
+	templateUrl: './tsteele-add-two-numbers.html',
+	styleUrl: './tsteele-add-two-numbers.css',
 })
 export class TsteeleAddTwoNumbers {
+	protected readonly numberOne = signal(2);
+	protected readonly numberTwo = signal(2);
+	protected readonly answer = signal(4);
 
+	protected readonly updateNumberOne = (newNumber: number) =>
+		this.numberOne.update((prev) => newNumber);
+
+	protected readonly addNumbers = () => {
+		console.log('User pressed equal button');
+		this.answer.update((prev) => this.numberOne() + this.numberTwo());
+	};
 }
