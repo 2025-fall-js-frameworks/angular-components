c137ea9b19851429da7aa6fbb4ccd94726755c09
Ola-Juwon <olajuwonsomuyiwa@gmail.com>
2025-10-30 13:11:06 -0500

    Added input text boxes and button to component html
    Added input text boxes and button to component html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { SwPlanetFilms } from './sw-planet-films/sw-planet-films';
+import { OlaMyNewComponent01 } from './ola-my-new-component-01/ola-my-new-component-01';
 
 @Component({
+	// print
 	selector: 'app-root',
-	imports: [SwPlanetFilms],
+
+	//Array of components
+	imports: [SwPlanetFilms, OlaMyNewComponent01],
 	templateUrl: './app.html',
 	styleUrl: './app.css',
 })
