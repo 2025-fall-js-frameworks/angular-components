d922ff3f683f871e593e6211950e0a8f1932cc5c
Ola-Juwon <olajuwonsomuyiwa@gmail.com>
2025-10-30 13:33:11 -0500

    Completed the calculator
    completed a functional calculator

--- src/app/ola-my-new-component-01/ola-my-new-component-01.ts
+++ src/app/ola-my-new-component-01/ola-my-new-component-01.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-ola-my-new-component-01',
-  imports: [],
-  templateUrl: './ola-my-new-component-01.html',
-  styleUrl: './ola-my-new-component-01.css'
+	selector: 'app-ola-my-new-component-01',
+	imports: [],
+	templateUrl: './ola-my-new-component-01.html',
+	styleUrl: './ola-my-new-component-01.css',
 })
 export class OlaMyNewComponent01 {
+	protected readonly numberOne = signal(2);
+	protected readonly numberTwo = signal(2);
+	protected readonly answer = signal(4);
 
+	// This lambda function get called when the button is clicked
+	protected readonly updaueNumberOne = (newNumber: number) =>
+		this.numberOne.update((prev) => newNumber);
+
+	protected readonly addNumbers = () => {
+		console.log('User pressed equal button');
+		this.answer.update((prev) => this.numberOne() + this.numberTwo());
+	};
 }
