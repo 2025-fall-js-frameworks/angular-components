37bcf81e9e6d0eefe7682a19ea10e0acfbc0b737
Ola-Juwon <olajuwonsomuyiwa@gmail.com>
2025-10-30 12:55:59 -0500

    Generated my new component
    ng generate component ola-myNew-component-01

---
+++ src/app/ola-my-new-component-01/ola-my-new-component-01.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ola-my-new-component-01',
+  imports: [],
+  templateUrl: './ola-my-new-component-01.html',
+  styleUrl: './ola-my-new-component-01.css'
+})
+export class OlaMyNewComponent01 {
+
+}
