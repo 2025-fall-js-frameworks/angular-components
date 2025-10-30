3801ee4977d8ad7240a5dd7cce8bec0dab4978b3
yyang22-JS Frameworks <yyang22@madisoncollege.edu>
2025-10-30 13:27:31 -0500

    Component working...
    Next create the PR...

--- src/app/yyang22-add-two-numbers/yyang22-add-two-numbers.ts
+++ src/app/yyang22-add-two-numbers/yyang22-add-two-numbers.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-yyang22-add-two-numbers',
-  imports: [],
-  templateUrl: './yyang22-add-two-numbers.html',
-  styleUrl: './yyang22-add-two-numbers.css'
+	selector: 'app-yyang22-add-two-numbers',
+	imports: [],
+	templateUrl: './yyang22-add-two-numbers.html',
+	styleUrl: './yyang22-add-two-numbers.css',
 })
 export class Yyang22AddTwoNumbers {
+	protected readonly numberOne = signal(2);
+	protected readonly numberTwo = signal(2);
+	protected readonly answer = signal(4);
 
+	protected readonly updateNumberOne = (newNumber: number) =>
+		this.numberOne.update((prev) => newNumber);
+
+	protected readonly addNumbers = () => {
+		console.log('User pressed equal button');
+		this.answer.update((prev) => this.numberOne() + this.numberTwo());
+	};
 }
