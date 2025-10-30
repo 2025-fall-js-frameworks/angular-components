5512f2a9a7a101844de3df120e23e105ba7cf2f7
matgeroina <157189615+matgeroina@users.noreply.github.com>
2025-10-30 12:56:03 -0500

    generated a new component
    comand line:
    ng generate component akoroliova-my-component-02

---
+++ src/app/akoroliova-my-component-02/akoroliova-my-component-02.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-akoroliova-my-component-02',
+  imports: [],
+  templateUrl: './akoroliova-my-component-02.html',
+  styleUrl: './akoroliova-my-component-02.css'
+})
+export class AkoroliovaMyComponent02 {
+
+}
