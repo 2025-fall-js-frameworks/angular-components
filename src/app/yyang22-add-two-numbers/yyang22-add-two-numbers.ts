84771dfb69f2b4c7ec92e5ee463b0a339e8a6184
yyang22-JS Frameworks <yyang22@madisoncollege.edu>
2025-10-30 12:55:58 -0500

    Generated my new component
    ng generate component yyang22-add-two-numbers

---
+++ src/app/yyang22-add-two-numbers/yyang22-add-two-numbers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-yyang22-add-two-numbers',
+  imports: [],
+  templateUrl: './yyang22-add-two-numbers.html',
+  styleUrl: './yyang22-add-two-numbers.css'
+})
+export class Yyang22AddTwoNumbers {
+
+}
