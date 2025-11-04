d3b7ffd76b715c7aa1eda4b95d71721cd376567c
jegyes <jegyes@madisoncollege.edu>
2025-11-04 11:45:11 -0600

    generated my new wk8 component

---
+++ src/app/jgyes-add-two-numbers/jgyes-add-two-numbers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jgyes-add-two-numbers',
+  imports: [],
+  templateUrl: './jgyes-add-two-numbers.html',
+  styleUrl: './jgyes-add-two-numbers.css'
+})
+export class JgyesAddTwoNumbers {
+
+}
