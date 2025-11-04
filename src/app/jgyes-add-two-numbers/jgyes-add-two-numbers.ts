a435847f9bb02ddc78b3ebd7ff9d1499e139a975
jegyes <jegyes@madisoncollege.edu>
2025-11-04 12:34:32 -0600

    component working

--- src/app/jgyes-add-two-numbers/jgyes-add-two-numbers.ts
+++ src/app/jgyes-add-two-numbers/jgyes-add-two-numbers.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: 'app-jgyes-add-two-numbers',
-  imports: [],
-  templateUrl: './jgyes-add-two-numbers.html',
-  styleUrl: './jgyes-add-two-numbers.css'
+	selector: 'app-jgyes-add-two-numbers',
+	imports: [],
+	templateUrl: './jgyes-add-two-numbers.html',
+	styleUrl: './jgyes-add-two-numbers.css',
 })
 export class JgyesAddTwoNumbers {
+	protected readonly numberOne = signal(2);
+	protected readonly numberTwo = signal(2);
+	protected readonly answer = signal(4);
 
+	protected readonly updateNumberOne = (newNumber: number) =>
+		this.numberOne.update((prev) => newNumber);
+
+	protected readonly addNumbers = () => {
+		console.log('user pressed equal button');
+		this.answer.update((prev) => this.numberOne() + this.numberTwo());
+	};
 }
