128d73b104821d7dcbb84bc72ee7f037be2afb9d
Austin-Spriggs <aspriggs@madisoncollege.edu>
2025-11-04 13:03:00 -0600

    added angular material "mat-card" styling to my component

--- src/app/sw-planet-population/sw-planet-population.ts
+++ src/app/sw-planet-population/sw-planet-population.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { SwPlanetsService } from '../sw-planets.service';
 import { PlanetWithFormattedPopulation } from '../../sw-planet-model';
+import { MatCardModule } from '@angular/material/card';
 
 
 @Component({
 	selector: 'app-sw-planet-population',
-	imports: [],
+	imports: [ MatCardModule ],
 	templateUrl: './sw-planet-population.html',
 	styleUrl: './sw-planet-population.css'
 })
