57b592b74dcb53589ab1032669dc2458ecf76b7d
tsteelematc <tsteele@madisoncollege.edu>
2025-11-04 13:38:30 -0600

    My second component styled and hooked up to service...

--- src/app/tsteele-my-component/tsteele-my-component.ts
+++ src/app/tsteele-my-component/tsteele-my-component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { SwPlanetsService } from '../sw-planets.service';
 
 @Component({
-  selector: 'app-tsteele-my-component',
-  imports: [],
-  templateUrl: './tsteele-my-component.html',
-  styleUrl: './tsteele-my-component.css'
+	selector: 'app-tsteele-my-component',
+	imports: [MatCardModule],
+	templateUrl: './tsteele-my-component.html',
+	styleUrl: './tsteele-my-component.css',
 })
 export class TsteeleMyComponent {
+	// Inject the service so we can get the planet climates...
+	private readonly planetSvc = inject(SwPlanetsService);
 
+	// Climates to display...
+	protected readonly planetClimates: string[] = this.planetSvc.getPlanetClimates();
 }
