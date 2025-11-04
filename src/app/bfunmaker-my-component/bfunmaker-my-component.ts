20b2240d1c1fa535271216f4c2f1ad8ce0869d0d
tsteelematc <tsteele@madisoncollege.edu>
2025-11-04 10:49:51 -0600

    MatSelect working and hooked up to service for list of climates...

--- src/app/bfunmaker-my-component/bfunmaker-my-component.ts
+++ src/app/bfunmaker-my-component/bfunmaker-my-component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { SwPlanetsService } from '../sw-planets.service';
 
 @Component({
 	selector: 'app-bfunmaker-my-component',
-	imports: [MatCardModule],
+	imports: [MatCardModule, MatFormFieldModule, MatSelectModule],
 	templateUrl: './bfunmaker-my-component.html',
 	styleUrl: './bfunmaker-my-component.css',
 })
-export class BfunmakerMyComponent {}
+export class BfunmakerMyComponent {
+	// Inject service, so we can load climates
+	protected readonly planetSvc: SwPlanetsService = inject(SwPlanetsService);
+
+	// Load climates into a property
+	protected readonly climates = this.planetSvc.getPlanetClimates();
+
+	// Also add a property for the selected climate
+}
