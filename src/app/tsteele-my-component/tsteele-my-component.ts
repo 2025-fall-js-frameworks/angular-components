bdb8a2ac744da3afef47aaf887b966c716eee07d
tsteelematc <tsteele@madisoncollege.edu>
2025-11-04 09:28:56 -0600

    Styled and hooked up my first sharing component...

--- src/app/tsteele-my-component/tsteele-my-component.ts
+++ src/app/tsteele-my-component/tsteele-my-component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { SwPlanetsService } from '../sw-planets.service';
 
 @Component({
-  selector: 'app-tsteele-my-component',
-  imports: [],
-  templateUrl: './tsteele-my-component.html',
-  styleUrl: './tsteele-my-component.css'
+	selector: 'app-tsteele-my-component',
+	imports: [MatCardModule],
+	templateUrl: './tsteele-my-component.html',
+	styleUrl: './tsteele-my-component.css',
 })
-export class TsteeleMyComponent {
+export class TsteeleMyComponent implements OnInit {
+	// Ctor auto property DI too... Used to be most popular before inject()...
+	constructor(private planetSvc: SwPlanetsService) {}
 
+	protected planetClimates: string[] = [];
+
+	ngOnInit(): void {
+		this.planetClimates = this.planetSvc.getPlanetClimates();
+	}
 }
