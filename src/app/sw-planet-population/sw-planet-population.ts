0512d1282aaebd169047182377c9ffad947bf125
Austin-Spriggs <aspriggs@madisoncollege.edu>
2025-11-04 13:29:11 -0600

    removed currency pipe

--- src/app/sw-planet-population/sw-planet-population.ts
+++ src/app/sw-planet-population/sw-planet-population.ts
@@ -3,12 +3,11 @@ import { SwPlanetsService } from '../sw-planets.service';
 import { PlanetWithFormattedPopulation } from '../../sw-planet-model';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
-import { CurrencyPipe } from '@angular/common';
 
 
 @Component({
 	selector: 'app-sw-planet-population',
-	imports: [ MatCardModule, MatListModule, CurrencyPipe ],
+	imports: [ MatCardModule, MatListModule ],
 	templateUrl: './sw-planet-population.html',
 	styleUrl: './sw-planet-population.css'
 })
