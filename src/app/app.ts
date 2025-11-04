310f4717023c0907279650caf6676f6a3455bec0
jegyes <jegyes@madisoncollege.edu>
2025-11-04 16:39:47 -0600

    regeneration of a component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { SwPlanetFilms } from './sw-planet-films/sw-planet-films';
+import { JegyesAddTwoNumbers } from './jegyes-add-two-numbers/jegyes-add-two-numbers';
 
 @Component({
 	selector: 'app-root',
-	imports: [SwPlanetFilms],
+	imports: [SwPlanetFilms, JegyesAddTwoNumbers],
 	templateUrl: './app.html',
 	styleUrl: './app.css',
 })
