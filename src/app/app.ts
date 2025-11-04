fc805628e480ff2cb33f3cff99ff715199c35b57
jegyes <jegyes@madisoncollege.edu>
2025-11-04 16:43:28 -0600

    debugging my regen

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { SwPlanetFilms } from './sw-planet-films/sw-planet-films';
-import { JegyesAddTwoNumbers } from './jegyes-add-two-numbers/jegyes-add-two-numbers';
+import { jegyesAddTwoNumbers } from './jegyes-add-two-numbers/jegyes-add-two-numbers';
 
 @Component({
 	selector: 'app-root',
-	imports: [SwPlanetFilms, JegyesAddTwoNumbers],
+	imports: [SwPlanetFilms, jegyesAddTwoNumbers],
 	templateUrl: './app.html',
 	styleUrl: './app.css',
 })
