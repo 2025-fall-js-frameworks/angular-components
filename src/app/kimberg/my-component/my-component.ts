8e04ddd27508cf238f2cd42c9a5acd0b14f8a56a
Kimberg23 <imbergk23@gmail.com>
2025-11-04 15:17:44 -0600

    Added Card

--- src/app/kimberg/my-component/my-component.ts
+++ src/app/kimberg/my-component/my-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
 	selector: 'app-my-component',
-	imports: [],
+	imports: [MatCardModule],
 	templateUrl: './my-component.html',
 	styleUrl: './my-component.css',
 })
