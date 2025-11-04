f496bfaf6d0273152245912267b8ef8fe6dacbe8
yyang22-JS Frameworks <yyang22@madisoncollege.edu>
2025-11-04 13:38:38 -0600

    My second component styled and hooked up to service...

--- src/app/yyang22-my-component/yyang22-my-component.ts
+++ src/app/yyang22-my-component/yyang22-my-component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { SwPlanetsService } from '../sw-planets.service';
 
 @Component({
-  selector: 'app-yyang22-my-component',
-  imports: [],
-  templateUrl: './yyang22-my-component.html',
-  styleUrl: './yyang22-my-component.css'
+	selector: 'app-yyang22-my-component',
+	imports: [MatCardModule],
+	templateUrl: './yyang22-my-component.html',
+	styleUrl: './yyang22-my-component.css',
 })
 export class Yyang22MyComponent {
-
+	// Inject the service so we can get the planet climates...
+	private readonly planetSvc = inject(SwPlanetsService);
+	// Climates to display....
+	protected readonly planetClimates: string[] = this.planetSvc.getPlanetClimates();
 }
