35f216b98406f6246bcb67397f57bb0d8f3cacf5
bryson-fun <funmaker.bryson@gmail.com>
2025-11-04 15:11:46 -0600

    Imported MatFormFieldModule, MatSelectModule, and injected SWPlanetService

--- src/app/bfunmaker-my-component/bfunmaker-my-component.ts
+++ src/app/bfunmaker-my-component/bfunmaker-my-component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { SwPlanetsService } from '../sw-planets.service';
 
 @Component({
 	selector: 'app-bfunmaker-my-component',
-	imports: [],
+	imports: [MatFormFieldModule, MatSelectModule],
 	templateUrl: './bfunmaker-my-component.html',
 	styleUrl: './bfunmaker-my-component.css',
 })
-export class BfunmakerMyComponent {}
+export class BfunmakerMyComponent {
+	selected = 'option2';
+
+	private readonly planetSvc = inject(SwPlanetsService);
+	// injject the service so we can get the planet climates
+	protected readonly planetClimates: string[] = this.planetSvc.getPlanetClimates();
+}
