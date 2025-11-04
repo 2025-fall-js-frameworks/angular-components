751109f21f063f42f274b25efb0bd9aa777be8fa
yyang22-JS Frameworks <yyang22@madisoncollege.edu>
2025-11-04 14:33:54 -0600

    Added another angular material

--- src/app/yyang22-my-component/yyang22-my-component.ts
+++ src/app/yyang22-my-component/yyang22-my-component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
+import { SwPlanetsService } from '../sw-planets.service';
 
 @Component({
 	selector: 'app-yyang22-my-component',
@@ -7,4 +8,7 @@ import { MatListModule } from '@angular/material/list';
 	templateUrl: './yyang22-my-component.html',
 	styleUrl: './yyang22-my-component.css',
 })
-export class Yyang22MyComponent {}
+export class Yyang22MyComponent {
+	private readonly planetSvc = inject(SwPlanetsService);
+	protected readonly planetClimates: string[] = this.planetSvc.getPlanetClimates();
+}
