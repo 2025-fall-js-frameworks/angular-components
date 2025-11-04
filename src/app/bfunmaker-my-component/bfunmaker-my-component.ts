b38c2dc0eb677196d1cd5598e14525987e3915ee
tsteelematc <tsteele@madisoncollege.edu>
2025-11-04 10:42:48 -0600

    MatCard with original placeholders...
    Next, MatSelect ? ? ?

--- src/app/bfunmaker-my-component/bfunmaker-my-component.ts
+++ src/app/bfunmaker-my-component/bfunmaker-my-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
 	selector: 'app-bfunmaker-my-component',
-	imports: [],
+	imports: [MatCardModule],
 	templateUrl: './bfunmaker-my-component.html',
 	styleUrl: './bfunmaker-my-component.css',
 })
