69137efc663b12202db74534dbb0498ee75f188d
yyang22-JS Frameworks <yyang22@madisoncollege.edu>
2025-11-04 14:24:02 -0600

    Adding a angular material to my component

--- src/app/yyang22-my-component/yyang22-my-component.ts
+++ src/app/yyang22-my-component/yyang22-my-component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
-  selector: 'app-yyang22-my-component',
-  imports: [],
-  templateUrl: './yyang22-my-component.html',
-  styleUrl: './yyang22-my-component.css'
+	selector: 'app-yyang22-my-component',
+	imports: [MatListModule],
+	templateUrl: './yyang22-my-component.html',
+	styleUrl: './yyang22-my-component.css',
 })
-export class Yyang22MyComponent {
-
-}
+export class Yyang22MyComponent {}
