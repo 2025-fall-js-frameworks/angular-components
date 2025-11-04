a2e23b6850aef236798713bc471103eb0edf64be
yyang22-JS Frameworks <yyang22@madisoncollege.edu>
2025-11-04 13:47:19 -0600

    Oh my, but it works... Di via Ctor injection....

--- src/app/yyang22-my-component/yyang22-my-component.ts
+++ src/app/yyang22-my-component/yyang22-my-component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { SwPlanetsService } from '../sw-planets.service';
 
@@ -8,9 +8,18 @@ import { SwPlanetsService } from '../sw-planets.service';
 	templateUrl: './yyang22-my-component.html',
 	styleUrl: './yyang22-my-component.css',
 })
-export class Yyang22MyComponent {
+export class Yyang22MyComponent implements OnInit {
 	// Inject the service so we can get the planet climates...
-	private readonly planetSvc = inject(SwPlanetsService);
+	// private readonly planetSvc = inject(SwPlanetsService);
+
+	// Ctor DI, with TS auto properties...
+	constructor(private planetSvc: SwPlanetsService) {}
+
 	// Climates to display....
-	protected readonly planetClimates: string[] = this.planetSvc.getPlanetClimates();
+	// protected readonly planetClimates: string[] = this.planetSvc.getPlanetClimates();
+	protected planetClimates: string[] = [];
+
+	ngOnInit(): void {
+		this.planetClimates = this.planetSvc.getPlanetClimates();
+	}
 }
