5872a2959a8110a8b92cc94f6293b93ec78c218d
ugursalih <uskayikci@gmail.com>
2025-11-06 13:11:00 -0600

    ugur/week-10-2
    css fix

--- src/app/ugur-my-component/ugur-my-component.ts
+++ src/app/ugur-my-component/ugur-my-component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { SwPlanetsService } from '../sw-planets.service';
 
 @Component({
   selector: 'app-ugur-my-component',
@@ -9,4 +10,21 @@ import { MatListModule } from '@angular/material/list';
   templateUrl: './ugur-my-component.html',
   styleUrls: ['./ugur-my-component.css']
 })
-export class UgurMyComponent {}
+export class UgurMyComponent {
+  private svc = inject(SwPlanetsService);
+
+  // Hocanın dediği gibi: servisten veri al + filtrele
+ 
+    planetsToDisplay = this.svc
+    .getPlanetFilmDataForDisplay()
+    .filter(p => {
+      const ch = p.name?.[0] ?? '';
+      return ch === 'T' || ch === 'N' || ch === 'C';
+    });
+  
+
+  // ngFor kullanmadan göstermek için listeyi string haline getiriyoruz
+  planetTextList = this.planetsToDisplay
+    .map(p => `${p.name} (${p.filmCount} films)`)
+    .join(', ');
+}
