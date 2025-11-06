e4f4c1f03fad98ee2ae6b49adb98a07fdcf1a76d
Matthew Nguyen <mnguyen4@madisoncollege.edu>
2025-11-06 13:31:29 -0600

    Hooked it up and added CSS framework

--- src/app/mnguyen-my-component/mnguyen-my-component.ts
+++ src/app/mnguyen-my-component/mnguyen-my-component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { SwPlanetsService } from '../sw-planets.service';
 
 @Component({
-  selector: 'app-mnguyen-my-component',
-  imports: [],
-  templateUrl: './mnguyen-my-component.html',
-  styleUrl: './mnguyen-my-component.css'
+	selector: 'app-mnguyen-my-component',
+	imports: [MatCardModule, MatListModule],
+	templateUrl: './mnguyen-my-component.html',
+	styleUrl: './mnguyen-my-component.css',
 })
 export class MnguyenMyComponent {
+	private planetSvc = inject(SwPlanetsService);
 
+	protected readonly swPlanets: String[] = this.planetSvc.planetNames;
+	protected readonly swPlanets2: String[] = this.planetSvc.planetNames;
+	protected readonly swPlanets3 = this.swPlanets.filter(
+		(planet) => !this.swPlanets2.includes(planet),
+	);
 }
