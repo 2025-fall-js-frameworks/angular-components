ce75b74e836b2c07595ac620688809123846f888
tsteelematc <tsteele@madisoncollege.edu>
2025-11-06 12:05:31 -0600

    Maybe this...

--- src/app/ugur-my-component/ugur-my-component.ts
+++ src/app/ugur-my-component/ugur-my-component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SwPlanetsService } from '../sw-planets.service';
 
 @Component({
-  selector: 'app-ugur-my-component',
-  imports: [],
-  templateUrl: './ugur-my-component.html',
-  styleUrl: './ugur-my-component.css'
+	selector: 'app-ugur-my-component',
+	imports: [],
+	templateUrl: './ugur-my-component.html',
+	styleUrl: './ugur-my-component.css',
 })
 export class UgurMyComponent {
+	// Inject the service.
+	private planetSvc = inject(SwPlanetsService);
 
+	// Use the service to load planets
+	// And filter and map to the one's you want
+	protected readonly planetsToDisplay = this.planetSvc
+		.getPlanetFilmDataForDisplay()
+		.filter((x) => ['Tatooine', 'Naboo', 'Coruscant'].includes(x.name))
+		.map((x) =>
+			x.name === 'Tatooine'
+				? 'Tatooine (desert, 2 suns)'
+				: x.name === 'Naboo'
+					? 'Naboo (lakes & medows)'
+					: 'Coruscant (city planet)',
+		);
 }
