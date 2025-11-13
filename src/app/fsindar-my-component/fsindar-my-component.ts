49699a48086b9303b95bbe313741968fa6c5393b
furkansindar <fsindar@madisoncollege.edu>
2025-11-13 11:04:56 -0600

    week 10 sharing
    I finish week 10 sharing

--- src/app/fsindar-my-component/fsindar-my-component.ts
+++ src/app/fsindar-my-component/fsindar-my-component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-fsindar-my-component',
-  imports: [],
+  imports: [MatListModule, MatCardModule],
   templateUrl: './fsindar-my-component.html',
   styleUrl: './fsindar-my-component.css'
 })
 export class FsindarMyComponent {
-
+   planetsWithoutHumans = [
+    {
+      name: 'Hoth',
+      description:
+        'An icy, uninhabitable world with no intelligent life; only creatures like wampas and tauntauns.'
+    },
+    {
+      name: 'Dagobah',
+      description:
+        'Swamp planet filled with creatures but no known civilization; Yoda lived there in exile.'
+    },
+    {
+      name: 'Endor',
+      description:
+        'Forest moon inhabited by Ewoks; no human civilization.'
+    }
+  ];
 }
